11a763286429a5585b791df923607b558f90c0f8
Perfect Tshibangu <jacquespt11@gamil.com>
2024-07-27 05:53:04 +0100

    creat siteone.html, bd.html and feuille.css

---
+++ script.js
@@ -0,0 +1,10 @@
+// script.js
+
+document.addEventListener('DOMContentLoaded', () => {
+    const hamburger = document.querySelector('.hamburger');
+    const navMenu = document.querySelector('.nav-menu');
+  
+    hamburger.addEventListener('click', () => {
+      navMenu.classList.toggle('active');
+    });
+  });
